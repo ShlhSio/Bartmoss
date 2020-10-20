91d9708106a4717ebf3a5be0f20df85f989dae2f
ShlhSio <71374237+ShlhSio@users.noreply.github.com>
2020-10-20 19:32:27 +0200

    Update Listes : Insérer.py

--- Listes : Insérer.py
+++ Listes : Insérer.py
@@ -2,8 +2,10 @@ def collec(collection1, collection2, position):
     if position < 0 or position > len(collection1):
         return None
     else:
-        collection1.insert(position-1, collection2)
-        return collection1
+        for n in range(len(collection2)):
+            n = collection2[n]
+            collection1.insert(position-1, n)
+    return collection1
 
 c1 = ['A', 'E', 'janvier', 7, 5]
 c2 = ["Mars", 'CCP', 'a3', 0]
